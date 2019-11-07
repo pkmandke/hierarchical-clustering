c8fd2afc86b8457da90f69f3ad6a842be41e06ba
Prathamesh Mandke <mandkepk97@gmail.com>
2019-11-07 17:37:05 -0500

    initial commit

---
+++ src/agglo_clus.py
@@ -0,0 +1,33 @@
+'''
+Agglomerative clustering
+
+'''
+import numpy as np
+from scipy.cluster.hierarchy import dendrogram, linkage
+from sklearn.cluster import AgglomerativeClustering as AC
+
+from datetime import timedelta
+import time
+
+import joblib
+
+class Agglo_clus:
+    
+    def __init__(self, tfidf_mtx, doc_names, num_clus=20, linkage='ward', affinity='euclidean'):
+        
+        self.tfidf = tfidf_mtx
+        self.doc_names = doc_names;
+        self.num_clus = num_clus
+        self.linkage = linkage
+        self.affinity = affinity
+        
+    def clusterize(self):
+        
+        print("Starting clustering...");
+        t1 = time.monotonic()
+        agg_clus = AC(n_clusters=self.num_clus, affinity=self.affinity, linkage=self.linkage)
+        self.predictions = agg_clus.fit_predict(self.tfidf)
+        print("Done training in {}s".format(timedelta(seconds=time.monotonic() - t1)))
+        
+    def save(self, name='agglo_clus_1.sav'):
+        joblib.dump(self, '../obj/' + name)
