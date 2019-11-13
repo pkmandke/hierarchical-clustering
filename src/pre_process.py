ef711e2db3cd4ae1e13265a0f457a2bf90cb5af6
Prathamesh Mandke <mandkepk97@gmail.com>
2019-11-13 00:19:14 +0000

    add main code for doc2vec of 5k docs w/ 128 len vector for 50 epochs

--- src/pre_process.py
+++ src/pre_process.py
@@ -19,7 +19,7 @@ class Doc2vec_wrapper:
 
     def __init__(self, json_path, n_docs=-1):
 
-        self.dframe = pd.read_json(json_path, orient=str, line=True)[['identifier-uri', 'description-abstract']]
+        self.dframe = pd.read_json(json_path, orient=str, lines=True)[['identifier-uri', 'description-abstract']]
         if n_docs == -1:
             self.tot_len = len(self.dframe)
         else:
@@ -54,7 +54,7 @@ class Doc2vec_wrapper:
 
         del self.dframe
     
-    def load_model_and_build_vocab(vector_size=20, min_count=2, epochs=20, workers=1):
+    def load_model_and_build_vocab(self, vector_size=20, min_count=2, epochs=20, workers=1):
 
         self.model = gensim.models.doc2vec.Doc2Vec(vector_size=vector_size, min_count=min_count, epochs=epochs, workers=workers)
         self.model.build_vocab(self.train_corpus)
@@ -63,6 +63,6 @@ class Doc2vec_wrapper:
 
         self.model.train(self.train_corpus, total_examples=self.model.corpus_count, epochs=self.model.epochs)
 
-    def save_model(self, path='../obj/doc2vec_'):
+    def save_model(self, path='../obj/abstracts_etd_doc2vec_'):
         path += str(self.tot_len) + '_docs'
         self.model.save(path)
