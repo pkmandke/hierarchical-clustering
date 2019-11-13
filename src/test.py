f0bf7bc84ab0e591fd12b6658001705dae21e6ec
Prathamesh Mandke <mandkepk97@gmail.com>
2019-11-13 17:35:39 -0500

    it's getting dark; launch the docker already! \n add unit test code and sample metadata. \n update .gitignore

--- src/test.py
+++ src/test.py
@@ -6,15 +6,15 @@ Test script
 from pre_process import Doc2vec_wrapper
 
 def main()
-    
-    doc2vec_model = Doc2vec_wrapper(json_path='/mnt/ceph/cme/30Ketds/30Kmetadata.json', n_docs=2)
-    
+
+    doc2vec_model = Doc2vec_wrapper(json_path='../obj/sample_metadata.json', n_docs=5)
+
     doc2vec_model.generate_tokens()
-    
+
     doc2vec_model.load_model_and_build_vocab(vector_size=128, epochs=1)
-    
+
     doc2vec_model.train()
-    
-    doc2vec_model.save_model()
-    
+
+    doc2vec_model.save_model(path='../obj/unittest_doc2vec_obj_')
+
 main()
